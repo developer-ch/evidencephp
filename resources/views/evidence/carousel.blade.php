3263b66128c48382ed4797e737ed1c306a27fec1
Carlos H <developerch@outlook.com>
2024-07-18 12:24:24 -0300

    Adjust

--- resources/views/evidence/carousel.blade.php
+++ resources/views/evidence/carousel.blade.php
@@ -8,12 +8,14 @@
     AGRUPADOR: <b>{{ $evidence->reference }}</b>
     <div class="carousel carousel-slider">
         <div class="carousel-item">
-            <img height="100%" src=" {{ asset('storage/' . $evidenceFile->file) }}?{{ rand() }}">
+            <img height="95%" src=" {{ asset('storage/' . $evidenceFile->file) }}?{{ rand() }}">
+            <p class="black-text center">{{$evidenceFile->description }}</p>
         </div>
         @foreach ($evidenceFiles as $f_evd)
             @if ($f_evd->id != $evidenceFile->id)
                 <div class="carousel-item">
-                    <img height="100%" width="100%" src=" {{ asset('storage/' . $f_evd->file) }}?{{ rand() }}">
+                    <img height="95%" width="100%" src=" {{ asset('storage/' . $f_evd->file) }}?{{ rand() }}">
+                    <p class="black-text center">{{ $f_evd->description }}</p>
                 </div>
             @endif
         @endforeach
