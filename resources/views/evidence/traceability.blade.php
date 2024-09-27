300f27e7b78628f265261ab1d2d0140076e02f0e
Carlos H <developerch@outlook.com>
2024-09-27 16:00:56 -0300

    Adjust traceability

--- resources/views/evidence/traceability.blade.php
+++ resources/views/evidence/traceability.blade.php
@@ -1,41 +1,26 @@
 @extends('layouts.main')
 @section('title', 'Evidence Logs')
 @section('page')
-<div class="title_box" id="subject">
-    <div id="title"><span style="font-size: 18px;text-align: center"><b>RASTREABILIDADE</b></span></div>
-    <a href="{{route('evidence.index')}}"
-        class="btn-floating right indigo darken-4 tooltipped" data-tooltip='Fechar Visualização' data-position="bottom">
-        <i class="small material-icons">close</i>
-    </a>
-    <p>Encontrei {{ $traceabilities->count() }} registros</p>
-    <div id="content" style="overflow:auto;height:85vh;overflow:auto;width:99.8%">
-        <table class="hide-on-med-and-down">
-            <thead>
-                <tr>
-                    <th>Mensagem</th>
-                    <th>Momento</th>
-                    <th>Autor</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($traceabilities as $log)
-                <tr>
-                    <td>{{ $log->message }}</td>
-                    <td>{{ date('d/m/Y H:i', strtotime($log->created_at)) }}</td>
-                    <td>{{ $log->user->name }} </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        <ul class="collection hide-on-large-only">
+<div>
+    <div class="navbar-fixed ">
+        <nav>
+            <div class="nav-wrapper indigo darken-4">
+                <form id="frm_search_traceability">
+                    <div class="input-field">
+                        <input id="search_traceability" type="search" placeholder="Procurar..." autofocus="autofocus" required>
+                        <label class="label-icon" for="search"><a href="{{route('evidence.index')}}" class="small tooltipped" data-tooltip='Voltar' data-position="bottom"><i class="material-icons">arrow_back</i></a></label>
+                    </div>
+                </form>
+            </div>
+        </nav>
+    </div>
+    <div>
+        <ul class="collection">
             @foreach ($traceabilities as $log)
             <li class="collection-item">
-                <span class="title"><b>Mensagem:</b> {{ $log->message }}</span>
-                <p>
-                    <b>Momento:</b> {{ date('d/m/Y H:i', strtotime($log->created_at)) }} <br>
-                    <b>Autor:</b> {{ $log->user->name }}
-                </p>
+                <span class="message"><b>Mensagem:</b> {{ $log->message }}</span><br>
+                <span class="moment"><b>Momento:</b> {{ date('d/m/Y H:i', strtotime($log->created_at)) }}</span><br>
+                <span class="autor"><b>Autor:</b> {{ $log->user->name }}</span>
             </li>
             @endforeach
         </ul>
@@ -45,5 +30,35 @@
 @endsection
 @push('scripts')
 <script>
+    const inputSearchTraceabilities = document.getElementById('search_traceability');
+    const containerList = document.querySelector("ul.collection");
+    const frmSearchTraceability = document.getElementById('frm_search_traceability');
+
+    frmSearchTraceability.addEventListener("submit", e => e.preventDefault());
+    
+    inputSearchTraceabilities.addEventListener("input", (e) => {
+        listTraceabilities(e.target.value);
+    });
+
+    const listTraceabilities = (valueSearch = "") => {
+        const MIN_TEXT_SEARCH = 1;
+        const traceabilities = document.querySelectorAll(".collection-item");
+        if (valueSearch.length >= MIN_TEXT_SEARCH) {
+            traceabilities.forEach(e => {
+                let txtEFilter = e.innerText;
+                if (txtEFilter.toUpperCase().includes(valueSearch.toUpperCase())) {
+                    e.style.display = "";
+                    txtEFilter = txtEFilter.replace(new RegExp(valueSearch, "gi"), (match) => {
+                        return `<strong style="color:red">${match}</strong>`
+                    })
+                } else {
+                    e.style.display = "none";
+                }
+                e.innerHTML = txtEFilter.replace('Mensagem:', "<b>Mensagem:</b>").replace('Momento:', "<br><b>Momento:</b>").replace('Autor:', "<br><b>Autor:</b>");
+            });
+        } else {
+            location.href = location.href;
+        }
+    }
 </script>
 @endpush
