e672ed5c92adad77eb0f92115f6b88279f05f6e3
Carlos H <developerch@outlook.com>
2024-10-10 16:49:33 -0300

    Update apresentation version

--- resources/views/evidence/modals/info.blade.php
+++ resources/views/evidence/modals/info.blade.php
@@ -7,10 +7,32 @@
             O "<b>EVIDENCE</b>" é um sistema, para agrupar arquivos de imagem, buscando facilitar a organização.
             <br /><br /><b>VERSÃO</b>
             <hr>
-            1.0.0
+            <span id="span-version"></span>
             <br /><br /><b>DESENVOLVEDOR</b>
             <hr>
             Carlos Henrique Mendes Lopes
         </div>
     </div>
 </div>
+@push('scripts')
+<script>
+    const spanVersion = document.getElementById("span-version")
+    const API_URL_BASE = "https://api.github.com/repos/developer-ch/evidencephp"
+
+    var options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+    options.timeZone = 'UTC';
+
+    (async () => {
+        const response = await fetch(API_URL_BASE)
+        const data = await response.json()
+        spanVersion.innerText = new Date(await data.pushed_at).toLocaleDateString('pt-BR', options)
+    })();
+</script>
+@endpush
